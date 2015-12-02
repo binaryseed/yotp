6fb75181ae9a7c49b4fb3f576d0188f14db0942b
binaryseed <vincefoley@gmail.com>
2015-12-01 21:12:01 -0800

    Remove ref

--- lib/step_5-yotp.ex
+++ lib/step_5-yotp.ex
@@ -9,16 +9,16 @@ defmodule YoTP do
 
   def loop(state) do
     receive do
-      {from, ref, query} ->
+      {from, query} ->
         {reply, new_state} = handle(query, state)
-        send from, {ref, reply}
+        send from, reply
         loop(new_state)
     end
   end
 
-  def call(pid, query, ref \\ make_ref) do
-    send pid, {self(), ref, query}
-    receive do {^ref, reply} -> reply end
+  def call(pid, query) do
+    send pid, {self(), query}
+    receive do reply -> reply end
   end
 
   # YoTP Callbacks
